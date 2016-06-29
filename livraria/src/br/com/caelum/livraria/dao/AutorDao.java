5af9cc38f7fb82111c99ff0cbb9a27b0c9b328e6
llcfromhell <llc.from.hell@gmail.com>
2016-06-28 23:52:47 -0300

    aula 04

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,43 +4,42 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class AutorDao {
 
 	@PostConstruct
 	void aposCriacao() {
-	    System.out.println("[INFO] AutorDao foi criado.");
+		System.out.println("[INFO] AutorDao foi criado.");
 	}
-	
+
 	@PersistenceContext
 	private EntityManager manager;
 
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public void salva(Autor autor) {
-	    System.out.println("[INFO] Salvando o Autor " + autor.getNome());
+		System.out.println("[INFO] Salvando o Autor " + autor.getNome());
 
-//	    try {
-//	        Thread.sleep(20000);    // 20 segundos
-//	    } catch (InterruptedException e) {
-//	        e.printStackTrace();
-//	    }
-
-	    manager.persist(autor);
-	    System.out.println("[INFO] Salvou o Autor " + autor.getNome());
+		manager.persist(autor);
+		System.out.println("[INFO] Salvou o Autor " + autor.getNome());
 	}
-	
+
 	public List<Autor> todosAutores() {
-		return manager.createQuery("select a from Autor a", Autor.class)
-	            .getResultList();
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
 		Autor autor = this.manager.find(Autor.class, autorId);
 		return autor;
 	}
-	
+
 }
